85826b7939d36af3506deb42822befa486219ec7
Gabriela <gabrielapenaherrera2014@gmail.com>
2018-07-05 21:13:21 -0500

    Datos-Arduino-GP

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-msft=pd.read_csv("arduino.csv",index_col=0)
+msft=pd.read_csv("DATOS_PROYECTO_SENSOR.csv",index_col=0)
 print(msft.head())
 
 print(msft.dtypes)
